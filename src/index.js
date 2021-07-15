7f71895ff613b1f86c942c958ca7d7df5f3f98bd
Mari Taksa <welch.mari@gmail.com>
2021-07-15 11:03:03 -0500

    Quiz Version Ready for Submission

--- src/index.js
+++ src/index.js
@@ -12,13 +12,22 @@ function displayQuiz(countries) {
     countries.forEach(country => {
         const countriesUL = document.querySelector('.countries-list')
         const countryName = document.createElement('li')
-        countryName.id = country.name
         countryName.textContent=`${country.name}`
+        countryName.id = country.name
+        let count = 0
         countryName.addEventListener('click', e => {
-            countryName.textContent=`In ${country.languages[0].name}, the country is called ${country.nativeName}`
+            count = count +1;
+            if (count%2 === 1) {
+                countryName.textContent=`In ${country.languages[0].name}, the country is called ${country.nativeName}`
+            }
+            else countryName.textContent=`${country.name}`  
         })
         countriesUL.append(countryName)
-        const userAnswer = document.createElement('form')
+        // const countryHiddenPart = document.createElement('p')
+        // countryName.append(countryHiddenPart)
+        // countryHiddenPart.textContent = ""
+        
+const userAnswer = document.createElement('form')
         userAnswer.addEventListener('submit', e => validateAnswer(e, country))
         const userAnswerInput = document.createElement('input')
         userAnswerInput.setAttribute('type', "text")
@@ -31,22 +40,20 @@ function displayQuiz(countries) {
     })   
 }
 //we know this one works, don't touch it
+
 function validateAnswer(e, country){
     e.preventDefault()
     if (e.target.userGuess.value === country.capital) {
-        document.getElementById(country.name).style.color = "green"}
+        document.getElementById(country.name).style.color = "green"
+    }
     else document.getElementById(country.name).style.color = "red"
+    // count = count + 1
+    // if (countryHiddenPart === 3){
+    //    console.log("hi") 
+    // }
     }
 
 //wrong answer three times reveals correct answer
-
-let count = 0
-function wrongAnswer(){
-    count = count + 1
-    if (count%3 === 1){
-        
-    }
-    return something 
-}    
+  
 
 //advanced feature: identify a close answer, i.e. accept upper and/or lowercase. later: only one letter is missing or letters are all present but not in correct positions
